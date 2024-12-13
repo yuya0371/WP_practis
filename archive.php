7cbe0ebb0ad908c5ed4b251c1f4b096ee9f25f1a
Yuya Osanai <osanaiyuya1229@gmail.com>
2024-12-13 12:03:03 +0900

    loop-post.phpを実装

--- archive.php
+++ archive.php
@@ -17,28 +17,7 @@
                         while(have_posts(  )):
                             the_post(  );
                         ?>
-                        <article id="post-<?php the_ID(  ); ?>" <?php post_class( 'module-Article-Item' ); ?>>
-                            <a href="<?php the_permalink(  ); ?>" class="module-Article_Item_Link">
-                                <div class="module-Article_Item_Img">
-                                    <?php if(has_post_thumbnail(  )): ?>
-                                        <?php the_post_thumbnail( 'archive_thumbnail' ); ?>
-                                    <?php else:?>
-                                        <img src="<?php echo esc_url(get_template_directory_uri(  )); ?>/assets/img/dummy-image.png" alt="" width="200" height="150" load="lazy">
-                                    <?php endif; ?>
-                                </div>
-                                <div class="module-Article_Item_Body">
-                                    <h2 class="module-Article_Item_Title"><?php the_title(  ); ?></h2>
-                                    <?php the_excerpt(  ); ?>
-
-                                    <ul class="module-Article_Item_Meta">
-                                        <li class="module-Article_Item_Cat">お知らせ</li>
-                                        <li class="module-Article_Item_Date">
-                                            <time datetime="<?php echo get_the_date( 'Y-m-d' ); ?>"><?php echo get_the_date(  ); ?></time>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </a>
-                        </article>
+                        <?php get_template_part( 'template-parts/loop', 'post' ); ?>
                         <?php endwhile; ?>
                         <?php endif; ?>
                         <nav class="navigation pagination" role="navigation" aria-label="投稿">
